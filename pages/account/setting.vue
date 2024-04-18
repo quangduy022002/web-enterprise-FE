b9a49588b9dcdc53f179f00b04d296078ee950ca
quangduy022002 <duy@ploggmedia.com>
2024-04-18 16:56:27 +0700

    chekc

--- pages/account/setting.vue
+++ pages/account/setting.vue
@@ -2,25 +2,34 @@
   <v-container>
     <v-card flat class="d-block justify-center align-center rounded-xl pa-8">
       <v-layout column justify-center align-center class="">
-        <v-img src="/logo.png" width="132" height="119" class="mb-4" />
+        <v-file-input id="inputFile" v-model="fileImage" style="visibility: hidden;" />
+        <v-badge
+          avatar
+          bordered
+          overlap
+          icon="mdi-camera"
+        >
+          <v-avatar size="132">
+            <v-img :src="avatar" @click="editImage" />
+          </v-avatar>
+        </v-badge>
+
         <h1 class="text-center">
           {{ $auth.user.email }}
         </h1>
       </v-layout>
 
       <v-card-text>
-        <v-form v-model="valid" @submit.prevent="signUp()">
-            <v-text-field
-              v-model="$auth.user.username"
-              class="rounded-xl"
-              color="black"
-
-              prepend-inner-icon="mdi-account"
-              outlined
-              label="Username"
-              :rules="[$rules.required]"
-              disabled
-            />
+        <v-form v-model="valid" @submit.prevent="editProfile()">
+          <v-text-field
+            v-model="$auth.user.username"
+            class="rounded-xl"
+            color="black"
+            prepend-inner-icon="mdi-account"
+            outlined
+            label="Username"
+            disabled
+          />
           <v-layout>
             <v-text-field
               v-model="form.firstName"
@@ -43,6 +52,38 @@
               :rules="[$rules.required]"
             />
           </v-layout>
+          <v-text-field
+            v-model="form.phone"
+            class="rounded-xl"
+            color="black"
+
+            prepend-inner-icon="mdi-account"
+            outlined
+            label="Phone Number"
+            :rules="[$rules.phone]"
+          />
+          <v-menu
+            bottom
+          >
+            <template #activator="{ on, attrs }">
+              <v-text-field
+                v-model="form.dob"
+                class="rounded-xl"
+                color="black"
+                prepend-inner-icon="mdi-account"
+                outlined
+                label="Date of birth"
+                v-bind="attrs"
+                v-on="on"
+              />
+            </template>
+            <v-date-picker v-model="form.dob" />
+          </v-menu>
+          <v-layout justify-end>
+            <v-btn type="submit" color="primary">
+              Save
+            </v-btn>
+          </v-layout>
         </v-form>
       </v-card-text>
     </v-card>
@@ -64,10 +105,20 @@ export default {
         address: undefined,
         avatar: undefined
       },
+      fileImage: undefined,
       loading: false,
       showPassword: false,
       showRetypedPassword: false,
       items: ['IT', 'Business', 'Design']
+    }
+  },
+  computed: {
+    avatar () {
+      if (this.fileImage) {
+        return URL.createObjectURL(this.fileImage)
+      } else {
+        return this.form.avatar || '/avatar.png'
+      }
     }
   },
   created () {
@@ -76,6 +127,22 @@ export default {
     })
   },
   methods: {
+    editImage () {
+      document.getElementById('inputFile').click()
+    },
+    async editProfile () {
+      if (this.fileImage) {
+        const data = new FormData()
+        data.append('files', this.fileImage)
+        const res = await this.$axios.post('/medias/upload', data, {
+          headers: {
+            'Content-Type': `multipart/form-data; boundary=${data._boundary}`
+          }
+        })
+        this.form.avatar = res.data[0]
+      }
+      await this.$axios.patch(`/account/update-info/${this.$auth.user.id}`, this.form)
+    }
   }
 }
 </script>
